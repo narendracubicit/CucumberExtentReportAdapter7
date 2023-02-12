dbf6b2613396db1dfb4cbbb92fb7ca06fbc61d23
narendracubicit <pallanarendra3@gmail.com>
2023-02-12 16:53:34 +0530

    updated extent properties and maven cucumber reporting plugin

--- src/main/java/com/narendra/automation/CucumberHooks.java
+++ src/main/java/com/narendra/automation/CucumberHooks.java
@@ -4,6 +4,7 @@ import com.narendra.automation.General.Driver;
 import io.cucumber.java.*;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.testng.Reporter;
 
 import java.io.IOException;
 
@@ -22,7 +23,10 @@ public class CucumberHooks implements Log4j {
 
     @AfterStep
     public static void afterEachStep(Scenario scenario) throws IOException {
-        scenario.attach(Utilities.getByteScreenshot(), "image/png", scenario.getName());
+//        scenario.attach(Utilities.getByteScreenshot(), "image/png", scenario.getName());
+        final byte[] screenshot = ((TakesScreenshot) Driver.createOnlineDriver()).getScreenshotAs(OutputType.BYTES);
+        scenario.attach(screenshot, "image/png",scenario.getName());
+
     }
 
     @After
